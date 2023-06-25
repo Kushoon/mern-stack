7f79f30624aabf844ebf4bf2718ddda2467264d9
Abhishek Serjam <abhishek.serjam@gmail.com>
2023-06-26 02:00:48 +0800

    Added both header and footer

--- frontend/src/components/FooterComponent.js
+++ frontend/src/components/FooterComponent.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 const FooterComponent = () => {
-  return <div>FooterComponent</div>;
+  return (
+    <footer>
+      <Container fluid>
+        <Row>
+          <Col className="bg-dark text-white text-center py-3">
+            Copyright &copy; BEST SHOP
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 };
 
 export default FooterComponent;
--- frontend/src/pages/ProductDetailsPage.js
+++ frontend/src/pages/ProductDetailsPage.js
@@ -1,5 +1,3 @@
-import { useParams } from "react-router-dom";
-
 const ProductDetailsPage = () => {
   return <div>ProductDetailsPage</div>;
 };
